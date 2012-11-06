f6e0cafb97f8e432fed14b6b82028fff6c32f655
David William Braithwaite <baixiwei@gmail.com>
2012-11-06 17:46:26 -0500

    Revisions by DWB 2012.11.06
    - TrialGenerator now keeps track of number of problems completed in each
    category and displays such at the top of each trial.
    - Quit button is not available until min number of problems in each
    category are completed.
    - TrialGenerator now generates data randomly instead of assuming it is
    provided beforehand. Content provided by Paulo should include
    specifications for how data should be generated in a given problem, e.g.
    max and min values, number of data points, etc.
    - TrialGenerator now generates actual correct answer instead of just a
    placeholder.
    - Feedback is now sensitive to whether or not correct answer was
    provided by participant.
    - Conditions are renamed using "related" and "unrelated" instead of
    "continuous" and "random".

--- startExperiment.js
+++ startExperiment.js
@@ -21,7 +21,7 @@ function runExperiment( display_loc, prepend_data, trial_generator, iter_num, op
         function( data ) {
             data        = $.extend( {}, prepend_data, { "trial_num": iter_num }, data );
             accum_data  = accum_data.concat( [ data ] );
-            if ( data.quit ) {
+            if ( data.option_text=="Quit" ) {
                 endExperiment( display_loc, accum_data );
             } else {
                 $.ajax( { type: 'post', cache: false, url: 'record_data.php',
@@ -61,9 +61,9 @@ function endExperiment( display_loc, accum_data ) {
 //////////////////////////////////////////////////////////////////////
 
 // TrialSpec class
-TrialSpec = function( text, question, feedback, options, data ) {
-    this.text       = text;
+TrialSpec = function( question, answer, feedback, options, data ) {
     this.question   = question;
+    this.answer     = answer;
     this.feedback   = feedback;
     this.options    = options;
     this.data       = data;
@@ -77,36 +77,40 @@ TrialSpec = function( text, question, feedback, options, data ) {
 function doTrial( display_loc, callback ) {
     var trial = this;
     // generate continue buttons and function for them to call on click
-    var continue_buttons = '';
+    var option_buttons = '';
     for ( var i=0; i<trial.options.length; i++ ) {
-        continue_buttons += '<button type="button" class="continue_buttons" id="continue_button_'+i+'">'+trial.options[i]+'</button>';
+        option_buttons += '<button type="button" class="option_buttons" id="option_button_'+i+'">'+trial.options[i]+'</button>';
     }
-    continue_buttons += '<button type="button" class="continue_buttons" id="continue_button_'+trial.options.length+'">Quit</button>';
     var returnResult = function( i ) {
         display_loc.html('');
         // pause?
         callback($.extend({},trial.data,
-            {"rt":(new Date()).getTime()-start_time,"response":response,"option":i,"quit":(i==trial.options.length)}));
+            {"rt":(new Date()).getTime()-start_time,"response":response,"option":i,"option_text":trial.options[i]}));
     }
     // post html to display_loc
 /*
 TBD (Josh?/David): the look of this is pretty primitive right now. I suspect this can be solved with css but I don't know from css - can Josh help with this?
 */
     display_loc.html( 
-        '<div id="text"><p>' + trial.text + '</p></div>' +
-        '<div id="question"><p>' + trial.question + '</p><p>' +
-          '<input type="text" id="answer_box"></input>' +
-          '<button type="button" id="answer_button">Show the answer</button></p></div>' +
-        '<div id="feedback"><p>' + trial.feedback + '</p></div>' +
-        '<div id="continue"><p>' + continue_buttons + '</p></div>'
+        '<div id="question"><p>' + trial.question + '</p></div>' +
+        '<div id="answer"><p><input type="text" id="answer_box"></input><button type="button" id="answer_button">Show the answer</button></p></div>' +
+        '<div id="feedback"></div>' +
+        '<div id="continue"><p>' + option_buttons + '</p></div>'
     );
-    $('.continue_buttons').click( function() { returnResult(Number(this.id.replace("continue_button_",""))); } );
+    $('.option_buttons').click( function() { returnResult(Number(this.id.replace("option_button_",""))); } );
     // hide feedback and submit buttons until user clicks answer_button
     $('#feedback').hide();
     $('#continue').hide();
     $('#answer_button').click( function() {
-        response = $('#answer_box').val();
         $('#answer_button').hide();
+        response = $('#answer_box').val();
+        if ( ( response==="" ) || ( response===undefined ) ) {
+            var correct = false;
+        } else {
+            var correct = ( (Math.round(response*100)/100)==(Math.round(trial.answer*100)/100) );
+        }
+        var feedback_text = "<p>" + trial.feedback[ correct ] + "</p>";
+        $('#feedback').html( feedback_text );
         $('#feedback').fadeIn(500);
         $('#continue').fadeIn(500);
     } );
@@ -121,82 +125,163 @@ TBD (Josh?/David): the look of this is pretty primitive right now. I suspect thi
 //////////////////////////////////////////////////////////////////////
 
 TrialGenerator = function( condition, items ) {
-/*
-TBD (Paulo/David): what follows is just a placeholder. Eventually trialspecs should be instantiated using content passed in via the items param. We should ensure that the number of stories and is the same for each category and the number of datasets is the same for each story. We should have enough stories and datasets that people are unlikely to exhaust them, but if people do, the code will respond gracefully by looping back to the beginning.
-*/
     // build mock trial content
     this.categories = [ "Mean", "Median", "Mode" ];
-    this.stories    = [ "Story 1", "Story 2", "Story 3" ];
-    this.datasets   = [ "Data Set A", "Data Set B", "Data Set C" ];
-    var feedback    = "Space filler for feedback.";
-    // convert trial content into trial specifications
-    this.trialspecs = new Array( this.categories.length );
-    var options;
-    for ( var i=0; i<this.categories.length; i++ ) {
-        this.trialspecs[i] = new Array( this.stories.length );
-        for ( var j=0; j<this.stories.length; j++ ) {
-            this.trialspecs[i][j] = new Array ( this.datasets.length );
-            for ( var k=0; k<this.datasets.length; k++ ) {
-                // eventually option text should be sensitive to condition
-                options = [
-                    ["See another example about <b>Mean</b>","See an example about <b>Median</b>","See an example about <b>Mode</b>"],
-                    ["See an example about <b>Mean</b>","See another example about <b>Median</b>","See an example about <b>Mode</b>"],
-                    ["See an example about <b>Mean</b>","See an example about <b>Median</b>","See another example about <b>Mode</b>"] ]
-                    [i];
-                this.trialspecs[i][j][k] = new TrialSpec(
-                    "<p>"+this.stories[j]+"</p><p>"+this.datasets[k]+"</p>",
-                    "Find the "+this.categories[i],
-                    feedback,
-                    options,
-                    {"category":i,"story":j,"dataset":k} );
 /*
-TBD (David): once we have more realistic content, we should add more detailed data (the last arg to the TrialSpec constructor above), e.g. the actual correct answer.
+TBD (Paulo/David): the story content below is just a placeholder. Eventually trialspecs should be instantiated using content passed in via the items param. We should ensure that the number of stories and is the same for each category. We should have enough stories that people are unlikely to exhaust them, but if people do, the code will respond gracefully by looping back to the beginning.
 */
-            }
-        }
+    this.stories    = [ "Story 1", "Story 2", "Story 3" ];
+    // minimum that must be completed per category in order to finish the worksheet
+    // and actual number completed for each category (initialized to 0)
+    var minimum_complete = 2;
+    this.minimums     = {};
+    this.completes    = {};
+    for ( var i=0; i<this.categories.length; i++ ) {
+        this.minimums[this.categories[i]] = minimum_complete;
+        this.completes[this.categories[i]] = 0;
     }
     this.condition    = condition;
     this.getNextTrial = getNextTrial;
+    this.getDataset   = getDataset;
+    this.getProgressBar = getProgressBar;
 }
 
 function getNextTrial( next_cat ) {
+    // select / generate the story and data for the next trial
     if ( next_cat===false ) {   // this is the first trial
         next_cat    = 0;
         next_story  = 0;
-        next_data   = 0;
+        data_relation = "random";
     } else {                    // at least one trial done before
-        if ( this.condition=="CC" ) {        // continuous within, continuous between
+        if ( this.condition=="RR" ) {        // related within, related between
             if ( this.prev_cat==next_cat ) {
-                var next_story = this.prev_story;
-                var next_data = (this.prev_data+1)%this.datasets.length ;
+                var next_story      = this.prev_story;
+                var data_relation   = "related";
             } else {
-                var next_story = this.prev_story;
-                var next_data = this.prev_data;
+                var next_story      = this.prev_story;
+                var data_relation   = "identical";
             }
-        } else if ( this.condition=="CR" ) { // continuous within, random between
+        } else if ( this.condition=="RU" ) { // related within, unrelated between
             if ( this.prev_cat==next_cat ) {
-                var next_story = this.prev_story;
-                var next_data = (this.prev_data+1)%this.datasets.length;
+                var next_story      = this.prev_story;
+                var data_relation   = "related";
             } else {
-                var next_story = (this.prev_story+1)%this.stories.length;
-                var next_data = this.prev_data;
+                var next_story      = (this.prev_story+1)%this.stories.length;
+                var data_relation   = "random";
             }
-        } else if ( this.condition=="RC" ) { // random within, continuous between
+        } else if ( this.condition=="UR" ) { // unrelated within, related between
             if ( this.prev_cat==next_cat ) {
-                var next_story = (this.prev_story+1)%this.stories.length;
-                var next_data = prev_data;
+                var next_story      = (this.prev_story+1)%this.stories.length;
+                var data_relation   = "random";
             } else {
-                var next_story = this.prev_story;
-                var next_data = this.prev_data;
+                var next_story      = this.prev_story;
+                var data_relation   = "identical";
             }
-        } else if ( this.condition=="RR" ) { // random within, random between
-            var next_story = (this.prev_story+1)%this.stories.length;
-            var next_data = prev_data;
+        } else if ( this.condition=="UU" ) { // unrelated within, unrelated between
+                var next_story      = (this.prev_story+1)%this.stories.length;
+                var data_relation   = "random";
         }
     }
-    this.prev_cat   = next_cat;
+    // record category selection; generate progress bar
+    this.prev_cat = next_cat;
+    this.completes[this.categories[next_cat]]++;
+    var bar = this.getProgressBar();
+    // record story selection; generate story text
     this.prev_story = next_story;
-    this.prev_data  = next_data;
-    return this.trialspecs[next_cat][next_story][next_data];
+    var story    = "<p>" + this.stories[ next_story ] + "</p>";
+    // generate data set, store array version in this and save text version for trial spec
+    D = this.getDataset( data_relation );
+    this.dataset = D["dataset"];
+    var dataset_text = D["HTML"];
+    // question
+    var measure = this.categories[ next_cat ];
+    var question = "<p>Find the " + measure + " of this data. (Round off to 2 decimal places.)</p>";
+    // feedback text
+    var answer = getCentTend( this.dataset, measure, 2 );
+    var feedback = { false: "<img src='small-red-x-mark-th.png'>  " + " Oops, that's not correct. The " + measure + " is " + answer + ".",
+                     true: "<img src='small-green-check-mark-th.png'>  " + " Yes, that's correct!" };
+    // do we need to give more explanation in the case of wrong answers?
+    // options buttons (include Quit only if minimum completes reached)
+    // eventually option text should be sensitive to condition
+    var options = [
+        ["See another example about <b>Mean</b>","See an example about <b>Median</b>","See an example about <b>Mode</b>"],
+        ["See an example about <b>Mean</b>","See another example about <b>Median</b>","See an example about <b>Mode</b>"],
+        ["See an example about <b>Mean</b>","See an example about <b>Median</b>","See another example about <b>Mode</b>"] ]
+        [next_cat];
+    var quit_option = true;
+    for ( var i=0; i<this.categories.length; i++ ) {
+        quit_option = ( quit_option && ( this.completes[this.categories[i]] >= this.minimums[this.categories[i]] ) );
+    }
+    if ( quit_option ) {
+        options.push( "Quit" );
+    }
+    // trial data to be recorded (as opposed to above "dataset" which is what is displayed to participant)
+    // once we have more realistic content, we should add more detailed data, e.g. the actual correct answer.
+    var data    = {"category":next_cat,"story":next_story,"dataset":this.dataset.toString()};
+    // generate and return the next trial spec
+    return new TrialSpec( bar + story + dataset_text + question, answer, feedback, options, data );
 }
 
+// generateData: method of TrialGenerator object
+//  generates a new data set and returns both array and HTML versions thereof
+//  if relation is random, new data is completely random within constraints of current value of this.story
+//  if relation is identical, new data is same as old data
+//  if relation is related, new data is a tweak of old data with changes highlighted in the HTML
+function getDataset( relation ) {
+    // currently does not work - the code below is just placeholder
+    var min=0; var max=3; var length=3;
+    var dataset = [];
+    for ( var i=0; i<length; i++ ) {
+        dataset.push( min + Math.floor( Math.random()*(max+1) ) );
+    }
+    var result = "<p>Data set: ";
+    for ( var i=0; i<dataset.length; i++ ) {
+        result += dataset[i] + ", ";
+    }
+    result = result.substring(0,result.length-2) + "</p>";
+    return { "dataset": dataset, "HTML": result };
+}
+
+function getCentTend( dataset, measure, precision ) {
+    if ( measure=="Mean" ) {
+        var result = dataset.reduce( function(a,b){return Number(a)+Number(b);} )/dataset.length;
+    } else if ( measure=="Median" ) {
+        if ( (dataset.length%2)==1 ) {
+            var result = dataset.sort()[(dataset.length-1)/2];
+        } else {
+            var result = ( dataset.sort()[dataset.length/2] + dataset.sort()[(dataset.length/2)-1] ) / 2;
+        }
+    } else if ( measure=="Mode" ) {
+        var freqs = {};
+        var max_freq = 0;
+        var el;
+        for ( var i=0; i<dataset.length; i++ ) {
+            el = dataset[i];
+            if ( freqs[el]!=undefined ) {
+                freqs[el]++;
+            } else {
+                freqs[el]=1;
+            }
+            if ( freqs[el]>max_freq ) {
+                max_freq=freqs[el];
+            }
+        }
+        var modal_els = [];
+        for ( var k in freqs ) {
+            if ( freqs[k]==max_freq ) {
+                modal_els.push( k );
+            }
+        }
+        var result = modal_els.reduce( function(a,b){return Number(a)+Number(b);} )/modal_els.length;
+    }
+    return Math.round(result*Math.pow(10,precision))/Math.pow(10,precision);
+}
+
+function getProgressBar() {
+    var bar = "<table border='1'><tr><td colspan='"+this.categories.length+"'>Number of problems completed for each type, including this problem:</td></tr><tr>";
+    for ( var i=0; i<this.categories.length; i++ ) {
+        bar += "<td><strong>"+this.categories[i]+":</strong><br>"+this.completes[this.categories[i]]+" / "+this.minimums[this.categories[i]]+"</td>";
+    }
+    bar += "</tr></table>";
+    return bar;
+}
